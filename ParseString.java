3012461c60a9869e1ebd03c4650313dfdae42457
Jesse Maki <jm361@gssdschools.ca>
2017-06-07 10:58:43 -0600

    Add files via upload

---
+++ ParseString.java
@@ -0,0 +1,27 @@
+/*
+ * ParseString.java
+ * Jesse Maki
+ * CSC30
+ * Start Date: Wednesday 7 June 2017
+ * This program simulates processing a line of text from a text file.
+ */
+ 
+ /**
+  The ParseString Class simulates processing a line of text from a text file.
+  */
+  public class ParseString {
+  	
+  	public static void main(String[] args){
+  	
+  		STring lineOfText = "Babyak,Akilah,2000,6,June";
+  		
+  	
+  		System.out.println("\n\t THE PARSE STRING SIMULATION PROGRAM \n");
+  		
+  		
+  		
+  		
+  	}
+  	
+  	
+}
